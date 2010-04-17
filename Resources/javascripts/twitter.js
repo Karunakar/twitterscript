255f262944bc5ebac03bfafed13aa0b2fe460595
Karunakar <revurikarna@gmail.com>
2010-04-18 01:37:32 +0530

    Fixed syntax errors

--- Resources/javascripts/twitter.js
+++ Resources/javascripts/twitter.js
@@ -1,8 +1,19 @@
 var Twitter = new function(){
 	// Private Global method
-var login
-}
+	var login 
 
-var Twitter = new function() {
+			//Private method for the make_base64_auth(user, password)
+
+			 function make_base64_auth(username, password) {
+				var ok = username + ':' + password;
+				var hash = Base64.encode(tok);
+				return "Basic " + hash;
+				}
+
+			//Public Section started here as per basic javascript OOPS
+			return {
+				login: function(username,password)
+			}
+
 }
 
